23a12af96ca5c5711356484565d0ded449f09d68
pepanfc <102806226+pepanfc@users.noreply.github.com>
2025-07-18 14:16:49 +0700

    Update new1.py

--- new1.py
+++ new1.py
@@ -146,7 +146,26 @@ def predict_structure(sequence):
         st.error("Failed to get 3D structure from server. Try again later.")
         return None
 
-
+def read_protein_sequences(file):
+    if os.path.exists(file) == False:
+        print('Error: file %s does not exist.' % file)
+        sys.exit(1)
+    with open(file,  errors='ignore') as f:
+        records = f.read()
+    if re.search('>', records) == None:
+        print('Error: the input file %s seems not in FASTA format!' % file)
+        sys.exit(1)
+    records = records.split('>')[1:]
+    fasta_sequences = []
+    for fasta in records:
+        array = fasta.split('\n')
+        header, sequence = array[0].split()[0], re.sub('[^ACDEFGHIKLMNPQRSTVWY-]', '', ''.join(array[1:]).upper())
+        header_array = header.split('|')
+        name = header_array[0]
+        #label = 'None' #header_array[1] if len(header_array) >= 1 else '0'
+        #label_train = 'None' #header_array[2] if len(header_array) >= 2 else 'training'
+        fasta_sequences.append([name, sequence])
+    return fasta_sequences
 
 LICENSE_TEXT = """
 <div style="border:2.5px solid #1A4B7A; border-radius:18px; padding:26px 22px; background:#f6fafd; margin:32px 0;">
